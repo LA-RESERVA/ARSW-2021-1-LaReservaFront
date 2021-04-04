4c942f7804e1cbe15477a4f76f2b8f7856c109a7
Brayan Burgos <brayan.burgos@mail.escuelaing.edu.co>
2021-04-03 21:04:17 -0500

    m10 conection back

---
+++ js/login.js
@@ -0,0 +1,21 @@
+var apiLogin = (function () {
+    function autenticarUsuario() {
+        var username = document.getElementById("username").value;
+        var password = document.getElementById("contraseña").value;
+        if (username === "" || password === "") {
+            alert("Todos los campos son requeridos.");
+        } else {
+            const options = {
+                method: 'GET',
+                url: "https://proyecto-arsw.herokuapp.com/usuarios/" +username + "/" + password
+            };
+            axios.request(options).then(function (response) {
+                if (response.data){
+                    window.location.href="/landingPropietarios.html";
+                }
+            }).catch(function (error) {
+                console.error(error);
+            });
+        }
+    }
+})();
